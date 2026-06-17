8bbd63321bd348f46c4f31b4bbac9313b6716b4d
Rebecca <maruscarebecca@gmail.com>
2026-06-18 00:36:22 +0300

    Added more pokemon names to words.js

--- src/words.js
+++ src/words.js
@@ -1,4 +1,6 @@
-const POKEMON = ['ekans', 'zubat', 'ditto', 'eevee', 'ralts', 'luxio', 'snivy'];
+const POKEMON = [
+    'ekans', 'zubat', 'ditto', 'eevee', 'ralts', 'luxio', 'snivy', 'absol', 'aipom', 'arbok', 'azelf', 'bagon', 'budew', 'burmy', 'doduo', 'entei', 'gible', 'gloom', 'golem', 'hypno', 'klink', 'kubfu', 'lotad', 'lugia', 'luxio', 'magby', 'minun', 'numel', 'paras', 'pichu', 'ralts', 'riolu', 'rotom', 'shinx'
+];
 
 export function getRandomPokemon() {
     return POKEMON[Math.floor(Math.random() * POKEMON.length)];
